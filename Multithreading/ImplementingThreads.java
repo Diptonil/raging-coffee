df22f37204c42833af14a0c0af541eddcbdac09d
Diptonil <diptonilr@gmail.com>
2022-07-18 02:24:06 +0530

    basic multithreading programs and merge, selection sort

---
+++ Multithreading/ImplementingThreads.java
@@ -0,0 +1,40 @@
+package Multithreading;
+
+public class ImplementingThreads {
+    public static void main(String args[]) {
+        ExampleThread childThread = new ExampleThread("Child Thread");
+        childThread.thread.start();
+        try {
+            for(int iterator = 0; iterator <= 5; iterator ++) {
+                System.out.println(iterator);
+                Thread.sleep(1000);
+            }
+        } catch(InterruptedException exception) {
+            System.out.println("Main thread interrupted.");
+        } finally {
+            System.out.println("Main thread exiting.");
+        }
+    }
+}
+
+class ExampleThread implements Runnable {
+    Thread thread;
+
+    ExampleThread(String threadName) {
+        thread = new Thread(this, threadName);
+        System.out.println(threadName + " thread has been created!");
+    }
+    
+    public void run() {
+        try {
+            for(int iterator = 0; iterator <= 5; iterator ++) {
+                System.out.println(iterator);
+                Thread.sleep(500);
+            }
+        } catch(InterruptedException exception) {
+            System.out.println("Child thread interrupted.");
+        } finally {
+            System.out.println("Child thread exiting.");
+        }
+    }
+}
---
+++ Multithreading/InheritingThreads.java
@@ -0,0 +1,39 @@
+package Multithreading;
+
+public class InheritingThreads {
+    public static void main(String args[]) {
+        ExampleThread childThread = new ExampleThread("Child Thread");
+        childThread.start();
+        try {
+            for(int iterator = 0; iterator <= 5; iterator ++) {
+                System.out.println(iterator);
+                Thread.sleep(1000);
+            }
+        } catch(InterruptedException exception) {
+            System.out.println("Main thread interrupted.");
+        } finally {
+            System.out.println("Main thread exiting.");
+        }
+    }
+}
+
+class ExampleThread extends Thread {
+
+    ExampleThread(String threadName) {
+        super(threadName);
+        System.out.println(threadName + " thread has been created!");
+    }
+
+    public void run() {
+        try {
+            for(int iterator = 0; iterator <= 5; iterator ++) {
+                System.out.println(iterator);
+                Thread.sleep(500);
+            }
+        } catch(InterruptedException exception) {
+            System.out.println("Child thread interrupted.");
+        } finally {
+            System.out.println("Child thread exiting.");
+        }
+    }
+}
