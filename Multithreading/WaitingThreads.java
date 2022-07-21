832b70f25aa909b717a5002c39358f744e86d2f0
Diptonil <diptonilr@gmail.com>
2022-07-21 21:13:03 +0530

    Linear search added

---
+++ Multithreading/WaitingThreads.java
@@ -0,0 +1,89 @@
+package multithreading;
+
+public class WaitingThreads {
+    public static void main(String[] args) {
+        ThreadContent threadContent = new ThreadContent();
+        ProducerThread producerThread = new ProducerThread(threadContent);
+        ConsumerThread consumerThread = new ConsumerThread(threadContent);
+
+        producerThread.thread.start();
+        consumerThread.thread.start();
+    }
+}
+
+class ProducerThread implements Runnable {
+    public Thread thread;
+    private String threadName;
+    private ThreadContent threadContent;
+
+    ProducerThread(ThreadContent threadContent) {
+        this.threadName = "Producer Thread";
+        thread = new Thread(this, threadName);
+        this.threadContent = threadContent;
+    }
+
+    public void run() {
+        for (int iterator = 0; ; iterator ++) {
+            threadContent.produce(iterator);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException exception) {
+                System.out.print("Producer thread interrupted.");
+            }
+        }
+    }
+}
+
+class ConsumerThread implements Runnable {
+    public Thread thread;
+    private String threadName;
+    private ThreadContent threadContent;
+
+    ConsumerThread(ThreadContent threadContent) {
+        this.threadName = "Consumer Thread";
+        thread = new Thread(this, threadName);
+        this.threadContent = threadContent;
+    }
+
+    public void run() {
+        for (int iterator = 0; ; iterator ++) {
+            threadContent.consume();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException exception) {
+                System.out.print("Consumer thread interrupted.");
+            }
+        }
+    }
+}
+
+class ThreadContent {
+    private int number;
+    private boolean flag = false;
+
+    synchronized public void produce(int number) {
+        while (flag){
+            try {
+                wait();
+            } catch (InterruptedException exception) {
+                System.out.println("Producer thread interrupted.");
+            }
+            this.number = number;
+            System.out.println("Produced value: " + number);
+            notify();
+        }
+    }
+
+    synchronized public void consume() {
+        while (!flag) {
+            try {
+                wait();
+            } catch (InterruptedException exception) {
+                System.out.println("Consumer thread interrupted.");
+            }
+            System.out.println("Consumed value: " + number);
+            flag = false;
+            notify();
+        }
+    }
+}
---
+++ arrays/DutchNationalFlag.java
@@ -0,0 +1,55 @@
+package arrays;
+
+import java.util.Random;
+
+public class DutchNationalFlag {
+    private static final int SIZE = 10;
+
+    public static void main(String[] args) {
+        int[] array = getArray();
+        array = transformArray(array);
+        displayArray(array);
+    }
+
+    private static int[] getArray() {
+        Random random = new Random();
+        int[] array = new int[SIZE];
+
+        for (int iterator = 0; iterator < SIZE; iterator ++) {
+            array[iterator] = random.nextInt(2);
+        }
+
+        return array;
+    }
+
+    private static int[] transformArray(int[] array) {
+        int leftPointer = 0;
+        int rightPointer = array.length - 1;
+        int midPointer = 0;
+        int copy;
+
+        while (midPointer <= rightPointer) {
+            if (array[midPointer] == 0) {
+                copy = array[midPointer];
+                array[midPointer] = array[leftPointer];
+                array[leftPointer] = copy;
+                leftPointer ++;
+                midPointer ++;
+            } else if (array[midPointer] == 1) {
+                midPointer ++;
+            } else if (array[midPointer] == 2) {
+                copy = array[midPointer];
+                array[midPointer] = array[rightPointer];
+                array[rightPointer] = copy;
+                rightPointer --;
+            }
+        }
+    }
+
+    public static void displayArray(int[] array) {
+        System.out.print("The sorted array is: ");
+        for (int item : array) {
+            System.out.print(item + "\t");
+        }
+    }
+}
